05e14cdd8bb2e7d9e78954a71b0fda27e89cdd53
sideeqhammed <sideeqhammed05.com>
2026-02-06 15:20:22 +0100

    Completed frontend only part

--- todo_list_fe/src/components/tabs.jsx
+++ todo_list_fe/src/components/tabs.jsx
@@ -7,27 +7,27 @@ function Tabs() {
   return(
     <div>
       <div className="flex justify-center p-3">
-        <div className="relative flex bg-white rounded-lg">
+        <div className="relative grid grid-cols-3 bg-white rounded-lg p-1">
           <div 
             className="absolute top-1 bottom-1 w-1/3 bg-gray-300 rounded-md transition-transform duration-300"
             style={{transform: `translateX(${active * 100}%)`}}
           >
           </div>
           <button
-            className="relative z-10 text-lg p-4 rounded-md"
+            className="relative z-10 text-sm sm:text-lg p-4 rounded-md "
             onClick={() => setActive(0)}
           >
             All Tasks
           </button>
           <button 
-            className="relative z-10 text-lg p-4 rounded-md"
+            className="relative z-10 text-sm sm:text-lg p-4 rounded-md"
             onClick={() => setActive(1)}
           >
             Pending
           </button>
           {/* <button>Pending</button> */}
           <button 
-            className="relative z-10 text-lg p-4 rounded-md"
+            className="relative z-10 text-sm sm:text-lg p-4 rounded-md"
             onClick={() => setActive(2)}
           >
             Completed
